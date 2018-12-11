1f01ec7d74a5286855fd908c79b629411ba46f30
Zotho <svjatoslavalekseef2@gmail.com>
2018-12-11 01:00:36 +0000

    Edited setup.py

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-sudo pacman -Sy --noconfirm yaourt htop
-yaourt -S --noconfirm sublime-text-dev python-numpy ipython
-sudo yaourt -Scc --noconfirm
-git init
-git config --global user.name "Zotho"
+sudo pacman -Sy --noconfirm yaourt htop &&
+yaourt -S --noconfirm sublime-text-dev python-numpy ipython &&
+sudo yaourt -Scc --noconfirm &&
+git init &&
+git config --global user.name "Zotho" &&
 git config --global user.email "svjatoslavalekseef2@gmail.com"
