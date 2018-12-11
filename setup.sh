652d11c93fbc10096dae2d300bab544435e5509d
Zotho <svjatoslavalekseef2@gmail.com>
2018-12-11 03:01:14 +0000

    Added links

--- setup.sh
+++ setup.sh
@@ -5,3 +5,15 @@ sudo yaourt -Scc --noconfirm &&
 git init &&
 git config --global user.name "Zotho" &&
 git config --global user.email "svjatoslavalekseef2@gmail.com"
+
+# 
+# http://robertour.com/2013/07/19/10-things-you-should-know-about-the-kivy-canvas/
+
+# 
+# https://github.com/amatelin/kivy-projects
+
+# 
+# https://github.com/amatelin/neural-networks/tree/master/Visual%20classification/digits
+
+# Kivy with Python tutorial Part 8 - Simple Drawing Application
+#  https://www.youtube.com/watch?v=U9TnXzQgIJU
