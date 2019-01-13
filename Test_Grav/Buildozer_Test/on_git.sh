b5faeb0a0e565a2efd823e30a4d8d430589d7776
Zotho <svjatoslavalekseef2@gmail.com>
2019-01-13 00:56:13 +0000

    Test

--- Test_Grav/Buildozer_Test/on_git.sh
+++ Test_Grav/Buildozer_Test/on_git.sh
@@ -2,5 +2,8 @@
 cd ../../
 git add *
 git commit -a -m $1
-git remote set-url origin git@github.com:zotho/geometry-np.git
+# git remote set-url origin git@github.com:zotho/geometry-np.git
+# eval "$(ssh-agent -s)"
+# ssh-keygen -t rsa
+# ssh-add ~/.ssh/id_rsa
 git push origin master
